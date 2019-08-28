449d8fcb5612f49886649ddf687a937856192cf4
sriley <sarahriley.dev@gmail.com>
2019-08-28 15:32:43 -0400

    add actions and reducers

---
+++ songs/src/reducers/index.js
@@ -0,0 +1,17 @@
+// reducers
+const songsReducer = () => {
+  return [
+    { title: "Country Roads", duration: "4:05" },
+    { title: "Blue Skies", duration: "6:07" },
+    { title: "Whatever It Takes", duration: "3:35" },
+    { title: "Hello", duration: "4:55" }
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+
+  return selectedSong;
+};
